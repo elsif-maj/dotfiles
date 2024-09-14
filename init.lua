b8cde114c1ff89a2df603f1c76995e2149328a5e
Michael Johnson <elsif.maj@gmail.com>
2024-09-14 16:38:51 -0700

    Deal with warning messages from which-key plugin

--- init.lua
+++ init.lua
@@ -294,20 +294,24 @@ require("lazy").setup({
 		config = function() -- This is the function that runs, AFTER loading
 			require("which-key").setup()
 
-			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-			})
-			-- visual mode
-			require("which-key").register({
-				["<leader>h"] = { "Git [H]unk" },
-			}, { mode = "v" })
+			-- MJ: commented out 9-14-24.  :checkhealth which-key shows suggested new format, but when
+			-- replacing these it messes things up. If I don't replace then annoying warning message
+			-- on opening nvim
+			--
+			-- -- Document existing key chains
+			-- require("which-key").register({
+			-- 	["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+			-- 	["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+			-- 	["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+			-- 	["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+			-- 	["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+			-- 	["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
+			-- 	["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+			-- })
+			-- -- visual mode
+			-- require("which-key").register({
+			-- 	["<leader>h"] = { "Git [H]unk" },
+			-- }, { mode = "v" })
 		end,
 	},
 
