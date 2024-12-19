9af8dec480efbf670174f8518ba8f9bfd5ab1104
Michael Johnson <elsif.maj@gmail.com>
2024-12-18 16:41:32 -0800

    NVIM COPILOT CHAT!

--- init.lua
+++ init.lua
@@ -804,6 +804,19 @@ require("lazy").setup({
 	{ "folke/tokyonight.nvim" },
 
 	-- MJ:
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
+
 	{ "https://github.com/p00f/alabaster.nvim" },
 	{ "https://github.com/jaredgorski/fogbell.vim" },
 	{ "https://github.com/huyvohcmc/atlas.vim" },
